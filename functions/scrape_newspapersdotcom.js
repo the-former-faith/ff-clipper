ca45f655370ed41fcc08df8c42f4a14cc20e518e
Matt Derocher <mjamesderocher@gmail.com>
2021-08-03 21:49:28 -0500

    Created upload function

---
+++ functions/scrape_newspapersdotcom.js
@@ -0,0 +1,19 @@
+//Might be able to replace Axios with Node Fetch
+const axios = require('axios');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+exports.handler = async function (event) {
+  page = axios.get('https://www.newspapers.com/clip/72082117/the-leon-journal-reporter').then(response => {
+    const dom = new JSDOM(response.data);
+    console.log(dom.window.document.querySelectorAll('#sl-image img')[0].src)
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({message: "Uploaded!", asset: page})
+  }
+}
